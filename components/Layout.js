6ebc505488376d2cd2bd8f352bdd6ff9ba34f810
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-28 11:10:53 -0300

    feat: add util style classes

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
+import useStyles from '../utils/styles'
 
 export default function Layout({ children }) {
+	const classes = useStyles()
 	return (
 		<div>
 			<Head>
 				<title>Next Ecommerce</title>
 			</Head>
-			<AppBar position='static'>
+			<AppBar position='static' className={classes.navbar}>
 				<Toolbar>
-					<Typography>Next Ecommerce </Typography>
+					<Typography> Next Ecommerce </Typography>
 				</Toolbar>
 			</AppBar>
-			<Container>{children}</Container>
+			<Container className={classes.main}>{children}</Container>
+			<footer className={classes.footer}>
+				<Typography> All rights reserved. Next Ecommerce. </Typography>
+			</footer>
 		</div>
 	)
 }
