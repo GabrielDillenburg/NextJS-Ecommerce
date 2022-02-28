60fc945c391269d02d7bd89c47c3922a0fa37049
GabrielDillenburg <dillenburg.gabriel@gmail.com>
2022-02-28 11:53:01 -0300

    feat: add product list components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
+import {
+	Grid,
+	Card,
+	CardActionArea,
+	CardMedia,
+	CardContent,
+	Typography,
+	CardActions,
+	Button,
+} from '@material-ui/core'
+import data from '../utils/data'
 
 export default function Home() {
 	return (
@@ -8,9 +19,29 @@ export default function Home() {
 			<div>
 				<h1> Products </h1>
 				<ul>
-					<li> Product 1</li>
-					<li> Product 2</li>
-					<li> Product 3</li>
+					<Grid container spacing={3}>
+						{data.products.map((product) => (
+							<Grid item md={4} key={product.name}>
+								<Card>
+									<CardActionArea>
+										<CardMedia
+											component='img'
+											image={product.image}
+											title={product.name}></CardMedia>
+										<CardContent>
+											<Typography>{product.name}</Typography>
+										</CardContent>
+									</CardActionArea>
+									<CardActions>
+										<Typography>{product.price}</Typography>
+										<Button size='small' color='primary'>
+											Add to cart
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
 				</ul>
 			</div>
 		</Layout>
